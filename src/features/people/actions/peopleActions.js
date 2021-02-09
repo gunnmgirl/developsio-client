39ca7d0a9d9e7a54161e11d015b48b8a8e740eb9
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2021-02-09 13:57:05 +0100

    DEV-120 Delete applicant feature (#21)

--- src/features/people/actions/peopleActions.js
+++ src/features/people/actions/peopleActions.js
@@ -40,3 +40,40 @@ const getApplicantsSuccess = (payload) => {
     payload,
   };
 };
+
+export const deleteApplicant = (payload) => {
+  return async (dispatch) => {
+    dispatch(deleteApplicantRequest());
+    try {
+      await request.delete(`/applicant/`, payload);
+      dispatch(deleteApplicantSuccess(payload.personId));
+      notify("Applicant was successfully deleted.", "success");
+    } catch (error) {
+      if (!error || !error.data) {
+        notify("Something went wrong, please try again later.", "error");
+      } else {
+        notify(error.data, "error");
+      }
+      dispatch(deleteApplicantFailure());
+    }
+  };
+};
+
+const deleteApplicantRequest = () => {
+  return {
+    type: "DELETE_APPLICANT_REQUEST",
+  };
+};
+
+const deleteApplicantFailure = () => {
+  return {
+    type: "DELETE_APPLICANT_FAILURE",
+  };
+};
+
+const deleteApplicantSuccess = (payload) => {
+  return {
+    type: "DELETE_APPLICANT_SUCCESS",
+    payload,
+  };
+};
